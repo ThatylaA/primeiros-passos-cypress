7efa0a94f746e7bcf7e899f1f8d55e316fec88db
ThatylaA <thatylaanchieta@gmail.com>
2025-04-07 05:15:41 -0300

    Adding user data json

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,17 +1,30 @@
+import userData from '../fixtures/users/userData.json'
+
 describe('Orange HRM Tests', () => {
+
+  const selectorsList = {
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
+    loginButton: "[type='submit']",
+    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb')",
+    wrongCredentialAlert: ".oxd-alert-content"
+  
+  }
+
   it('Login - Success', () => {
+    
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type('Admin')
-    cy.get("[name='password']").type('admin123')
-    cy.get("[type='submit']").click()
+    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
+    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
+    cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text')
+    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard')
 })
 it('Login - Fail', () => {
   cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-  cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Test')
-  cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('test123')
-  cy.get('.oxd-button').click()
-  cy.get('.oxd-alert-content')
+  cy.get(selectorsList.usernameField).type(userData.userFail.username)
+  cy.get(selectorsList.passwordField).type(userData.userFail.password)
+  cy.get(selectorsList.loginButton).click()
+  cy.get(selectorsList.wrongCredentialAlert)
 })
 })
